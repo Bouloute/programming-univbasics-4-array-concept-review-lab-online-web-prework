3164e3a6484c78505483b0f76c94929ce5825f5e
Annaick Lewis Dagnino <annaick1995@gmail.com>
2019-11-12 04:18:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   
   while (index < array.length) do
     #if (array[index] === value_to_find) ? return index : index += 1
-    #return index if (array[index] === value_to_find) : index += 1 
+    #return index if (array[index] === value_to_find) ? index += 1 
     if (array[index] === value_to_find) 
       return index
     end
